adf34ea28b4b2ff06d44169a8a938c2925b0dd1e
AtlasWongy <atlasyjyj@gmail.com>
2023-02-20 23:41:58 +0800

    Init Login Page

---
+++ client/src/components/Title/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className='title-block'>
+            <h1 className = 'Title'> Market Place </h1>
+        </div>
+    )
+}
